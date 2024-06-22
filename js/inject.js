adb7b84670bec2a504a8a13f14600fefa8d2ab25
Thomas <thomastafernaberry@gmail.com>
2024-06-22 13:34:07 -0300

    Move translate injection from 'language.js'.

--- js/inject.js
+++ js/inject.js
@@ -1,46 +1,72 @@
-import { getData } from "./weather.js";
-import { appLanguage } from "./language.js";
+import { getWeatherData } from "./weather.js";
+import { appCurrentLanguage } from "./language.js";
 
-const $CITY = document.getElementById('city');
-const $CURRENT_TEMP = document.getElementById('current-temperature');
-const $CURRENT_HUMIDITY = document.getElementById('current-humidity');
-const $CURRENT_WEATHER = document.getElementById('weather');
-const $CURRENT_WIND_SPEED = document.getElementById('wind-speed');
-const $CURRENT_PRECIP_PROB = document.getElementById('precipitation-probability');
-const $CURRENT_APPARENT_TEMP = document.getElementById('current-apparent-temperature');
-const $FUN_FACT = document.getElementById('fun-fact');
-const $CURRENT_DATE = document.getElementById('current-date');
-// const $PREVIOUS_FACT_BUTTON = document.getElementById('previous-fact-button');
-// const $NEXT_FACT_BUTTON = document.getElementById('next-fact-button');
-
-function cityParser(timezone) {
+function timezoneCityParser(timezone) {
     const lastSlashIndex = timezone.lastIndexOf('/');
     const city = timezone.slice(lastSlashIndex + 1).replace('_', ' ');
     return city;
 }
 
-async function replaceElements() {
-    const data = await getData();
+document.addEventListener('DOMContentLoaded', () => {
+    const $ = (e) => document.getElementById(e);
+    // Definitions' names that are DOM elements start with $ 
+    const $CITY = $('city');
+    const $CURRENT_TEMP = $('current-temperature');
+    const $CURRENT_HUMIDITY = $('current-humidity');
+    const $CURRENT_WEATHER = $('weather');
+    const $CURRENT_WIND_SPEED = $('wind-speed');
+    const $CURRENT_PRECIP_PROB = $('precipitation-probability');
+    const $CURRENT_APPARENT_TEMP = $('current-apparent-temperature');
+    const $FUN_FACT = $('fun-fact');
+    const $CURRENT_DATE = $('current-date');
+    // const $PREVIOUS_FACT_BUTTON = $('previous-fact-button');
+    // const $NEXT_FACT_BUTTON = $('next-fact-button');
+    
+    // $TITLE is used for to-be-translated text
+    const $TITLE_CURRENT_HUMIDITY = $('title-current-humidity');
+    const $TITLE_CURRENT_WIND = $('title-current-wind');
+    const $TITLE_CURRENT_RAIN = $('title-current-rain');
+    const $TITLE_CURRENT_APPARENT_TEMP = $('title-current-apparent-temperature');
+    const $TITLE_TODAY_TEMPERATURE = $('title-today-temperature');
+    const $TITLE_DID_YOU_KNOW = $('title-did-you-know');
+    // const $TITLE_FUN_WEATHER_FACTS = $('title-fun-weather-facts');
+    // const $TITLE_FOUR_DAYS_PRONOSTIC = $('title-four-days-pronostic');
 
-    const currentDate = new Date(data.current.time);
-    const options = {
-        weekday: 'long',
-        month: 'long',
-        day: 'numeric'
+    async function injectWeatherDataInDOM() {
+        const data = await getWeatherData();
+        const currentWeather = data.current;
+        const currentUnits = data.current_units;
+        const currentDate = new Date(data.current.time);
+        const options = {
+            weekday: 'long',
+            month: 'long',
+            day: 'numeric'
+        }
+        console.log(appCurrentLanguage);
+        // Weather data.
+        $CURRENT_DATE.innerText = `${currentDate.toLocaleString(navigator, options)}`;
+        $CITY.innerText = timezoneCityParser(data.timezone);
+        $CURRENT_TEMP.innerText = `${currentWeather.temperature_2m}${currentUnits.temperature_2m}`;
+        $CURRENT_HUMIDITY.innerText = `${currentWeather.relative_humidity_2m}${currentUnits.relative_humidity_2m}`;
+        $CURRENT_WEATHER.innerText = `${appCurrentLanguage.weatherCodeDescription[currentWeather.weather_code]}`;
+        $CURRENT_WIND_SPEED.innerText = `${currentWeather.wind_speed_10m}${currentUnits.wind_speed_10m}`;
+        $CURRENT_PRECIP_PROB.innerText = `${currentWeather.precipitation_probability}${currentUnits.precipitation_probability}`;
+        $CURRENT_APPARENT_TEMP.innerText = `${currentWeather.apparent_temperature}${currentUnits.apparent_temperature}`;
+        $FUN_FACT.innerText = appCurrentLanguage.facts[Math.floor((Math.random() * appCurrentLanguage.facts.length))];
+        // Text translation.
+        $TITLE_CURRENT_HUMIDITY.innerText = appCurrentLanguage.humidity;
+        $TITLE_CURRENT_WIND.innerText = appCurrentLanguage.wind;
+        $TITLE_CURRENT_RAIN.innerText = appCurrentLanguage.rain;
+        $TITLE_CURRENT_APPARENT_TEMP.innerText = appCurrentLanguage.apparentTemp;
+        $TITLE_TODAY_TEMPERATURE.innerText = appCurrentLanguage.todayTemp;
+        $TITLE_DID_YOU_KNOW.innerText = appCurrentLanguage.didYouKnow;
+        // $TITLE_FUN_WEATHER_FACTS.innerText = language.funWeatherFacts;
+        // $TITLE_FOUR_DAYS_PRONOSTIC.innerText = language;
     }
 
-    $CURRENT_DATE.innerText = `${currentDate.toLocaleString(navigator, options)}`;
-    $CITY.innerText = cityParser(data.timezone);
-    $CURRENT_TEMP.innerText = `${data.current.temperature_2m}${data.current_units.temperature_2m}`;
-    $CURRENT_HUMIDITY.innerText = `${data.current.relative_humidity_2m} ${data.current_units.relative_humidity_2m}`;
-    $CURRENT_WEATHER.innerText = `${appLanguage.weatherCodeDescription[data.current.weather_code]}`;
-    $CURRENT_WIND_SPEED.innerText = `${data.current.wind_speed_10m} ${data.current_units.wind_speed_10m}`;
-    $CURRENT_PRECIP_PROB.innerText = `${data.current.precipitation_probability} ${data.current_units.precipitation_probability}`;
-    $CURRENT_APPARENT_TEMP.innerText = `${data.current.apparent_temperature} ${data.current_units.apparent_temperature}`;
-    $FUN_FACT.innerText = appLanguage.facts[Math.floor((Math.random() * appLanguage.facts.length))];
-}
+    injectWeatherDataInDOM();
+})
 
-replaceElements();
 
 // // TODO DEBUG FACT BUTTONS
 
